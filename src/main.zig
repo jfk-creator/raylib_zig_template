c2ee0ab93b563fb02966bc2f54cabb73823a5abf
Julian Kliebhan <hallo@jkliebhan.de>
2026-01-01 10:57:14 +0100

    init

---
+++ src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+const screenWidth = 1280;
+const screenHeight = 720;
+
+pub fn main() !void { 
+    rl.initWindow(screenWidth, screenHeight, "Template");
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+       rl.beginDrawing();
+       {
+            rl.clearBackground(rl.Color.black);
+            rl.drawFPS(20, 20);
+       }
+       rl.endDrawing();
+    }
+}
+
